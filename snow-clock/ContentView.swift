4f4c820e6f126a797e4b7ed70c600022d3dfb069
hi2gage <hi2gage.pdx@gmail.com>
2021-12-07 01:58:54 -0700

    Initial Commit

---
+++ snow-clock/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  snow-clock
+//
+//  Created by Gage Halverson on 12/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ snow-clock/snow_clockApp.swift
@@ -0,0 +1,17 @@
+//
+//  snow_clockApp.swift
+//  snow-clock
+//
+//  Created by Gage Halverson on 12/7/21.
+//
+
+import SwiftUI
+
+@main
+struct snow_clockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
